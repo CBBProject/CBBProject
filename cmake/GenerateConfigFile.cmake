7353d9bb8b223bef139e27d7457159e8d888a09f
Theodore Papadopoulo <Theodore.Papadopoulo@inria.fr>
2014-02-12 20:16:09 +0100

    Various windows corrections.

--- cmake/GenerateConfigFile.cmake
+++ cmake/GenerateConfigFile.cmake
@@ -2,10 +2,9 @@
 
 get_property(LIB64 GLOBAL PROPERTY FIND_LIBRARY_USE_LIB64_PATHS)
 
-if (${LIB64} STREQUAL "TRUE")
+set(LIBSUFFIX "")
+if ("${LIB64}" STREQUAL "TRUE")
     set(LIBSUFFIX 64)
-else()
-    set(LIBSUFFIX "")
 endif()
 
 set(INSTALL_LIB_DIR     lib${LIBSUFFIX}/Odyssee++ CACHE PATH "Installation directory for libraries")
